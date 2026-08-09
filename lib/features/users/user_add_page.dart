94a7005affba55344b691d31747e4e98d43663ca
EsraYamanCode <yamaann.esra@gmail.com>
2026-08-09 23:43:33 +0300

    Kullanıcı ekleme sayfası eklendi, bu sayfa ile kullanıcı eklenebilecek.

---
+++ lib/features/users/user_add_page.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import '../../core/validators/user_validator.dart';
+
+class UserAddPage extends StatefulWidget {
+  const UserAddPage({super.key});
+
+  @override
+  State<UserAddPage> createState() => _UserAddPageState();
+}
+
+class _UserAddPageState extends State<UserAddPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  final _usernameController = TextEditingController();
+  final _userTitleController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  bool _obscurePassword = true;
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _userTitleController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  void _submitForm() {
+    if (!_formKey.currentState!.validate()) {
+      return;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('ADD USER')), // + butonunda Navigator.push yaptığımız için Flutter yeni sayfayı navigation stack'e ekledi;
+      // appBar'da bu sayfanın geri dönülebilir olduğunu anlayıp geri okunu otomatik olarak gösterdi ve geri yaptığımızda girdiğimiz sayfadan çıkıp stack'ten çıkarıyor.
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              
+              TextFormField(
+                controller: _usernameController,
+                decoration: const InputDecoration(
+                  labelText: 'Username',
+                  hintText: 'Kullanıcı giriş adı',
+                  border: OutlineInputBorder(),
+                ),
+                validator: UserValidator.validateUsername,
+              ),
+
+              const SizedBox(height: 16),
+              TextFormField(
+                controller: _userTitleController,
+                decoration: const InputDecoration(
+                  labelText: 'UserTitle',
+                  hintText: 'Kullanıcı adı',
+                  border: OutlineInputBorder(),
+                ),
+                validator: UserValidator.validateUserTitle,
+              ),
+
+              const SizedBox(height: 16),
+              TextFormField(
+                controller: _passwordController,
+                obscureText: _obscurePassword,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  hintText: 'Kullanıcı şifresi',
+                  border: const OutlineInputBorder(),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _obscurePassword
+                          ? Icons.visibility
+                          : Icons.visibility_off,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _obscurePassword = !_obscurePassword;
+                      });
+                    },
+                  ),
+                ),
+                validator: UserValidator.validatePassword,
+              ),
+
+              const SizedBox(height: 24),
+              ElevatedButton(
+                onPressed: _submitForm,
+                child: const Text('ADD USER'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
