6b3e50bddf5333dce83e395270b49b5c549ff915
EsraYamanCode <yamaann.esra@gmail.com>
2026-08-14 15:44:29 +0300

    category_page dosyası yeniden oluşturuldu ve içindeki sayfa StatelessWidget ile değiştirildi.

---
+++ lib/features/categories/category_page.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_deneme/features/categories/category_provider.dart';
+import 'package:provider/provider.dart';
+import 'category_add_page.dart';
+
+class CategoryManagement extends StatelessWidget {
+  const CategoryManagement({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Center(child: Text("Category Management"))),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Expanded(
+              child: Consumer<CategoryProvider>(
+                builder: (context, provider, child) {
+                  final categories = provider.categories;
+
+                  return ListView.builder(
+                    itemCount: categories.length,
+                    itemBuilder: (context, index) {
+                      final category = categories[index];
+
+                      return Card(
+                        child: ListTile(
+                          leading: CircleAvatar(child: Text("${index + 1}")),
+                          title: Text(category.CategoryTitle),
+                          trailing: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              IconButton(
+                                icon: const Icon(
+                                  Icons.edit,
+                                  color: Colors.blue,
+                                ),
+                                onPressed: () {},
+                              ),
+                              IconButton(
+                                icon: const Icon(
+                                  Icons.delete,
+                                  color: Colors.red,
+                                ),
+                                onPressed: () {},
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+            IconButton(
+              icon: const Icon(Icons.add, color: Colors.deepPurple),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const CategoryAddPage(),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
