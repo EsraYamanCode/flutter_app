f351f1e202cd88cd92f94536fdecd8720adc9aaf
EsraYamanCode <yamaann.esra@gmail.com>
2026-08-14 15:45:49 +0300

    category sayfasında ekleme butonu için category_add_page dosyası oluşturuldu.

---
+++ lib/features/categories/category_add_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CategoryAddPage extends StatefulWidget {
+  const CategoryAddPage({super.key});
+
+  @override
+  State<CategoryAddPage> createState() => _CategoryAddPage();
+}
+
+class _CategoryAddPage extends State<CategoryAddPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
