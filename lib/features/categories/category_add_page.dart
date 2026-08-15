ee05dd13d0730bb985f09f2e7c6b280345780cbe
EsraYamanCode <yamaann.esra@gmail.com>
2026-08-15 18:15:32 +0300

    kategori eklemek için gerekli inputlar eklendi ve gerekli fonksiyonlar oluşturuldu.

--- lib/features/categories/category_add_page.dart
+++ lib/features/categories/category_add_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_deneme/core/validators/category_validator.dart';
+import 'package:flutter_deneme/data/models/category_model.dart';
+import 'package:flutter_deneme/features/categories/category_provider.dart';
+import 'package:provider/provider.dart';
 
 class CategoryAddPage extends StatefulWidget {
   const CategoryAddPage({super.key});
@@ -8,8 +12,97 @@ class CategoryAddPage extends StatefulWidget {
 }
 
 class _CategoryAddPage extends State<CategoryAddPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  final _categoryTitleController = TextEditingController();
+  final _categoryDescriptionContoller = TextEditingController();
+  bool _status = true;
+
+  @override
+  void dispose() {
+    _categoryTitleController.dispose();
+    _categoryDescriptionContoller.dispose();
+    super.dispose();
+  }
+
+  void _submitForm() {
+    if (!_formKey.currentState!.validate()) {
+      return;
+    }
+
+    final categoryTitle = _categoryTitleController.text.trim();
+    final categoryDescription = _categoryDescriptionContoller.text.trim();
+
+    final provider = context.read<CategoryProvider>();
+
+    if (provider.isTitleExists(categoryTitle)) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('This title already taken, please try another title'),
+        ),
+      );
+      return;
+    }
+
+    final category = CategoryModel(
+      categoryId: DateTime.now().millisecondsSinceEpoch.toString(),
+      categoryTitle: categoryTitle,
+      categoryDescription: categoryDescription,
+    );
+
+    provider.addCategory(category);
+    Navigator.pop(context);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: const Text('ADD CATEGORY')),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextFormField(
+                controller: _categoryTitleController,
+                decoration: const InputDecoration(
+                  labelText: 'CategoryTitle',
+                  border: OutlineInputBorder(),
+                ),
+                validator: CategoryValidator.validateCategoryTitle,
+              ),
+              const SizedBox(height: 16),
+              TextFormField(
+                controller: _categoryDescriptionContoller,
+                decoration: const InputDecoration(
+                  labelText: 'CategoryDescription',
+                  border: OutlineInputBorder(),
+                ),
+                validator: CategoryValidator.validateCategoryTitle,
+              ),
+              const SizedBox(height: 16),
+              CheckboxListTile(
+                title: Text(_status ? 'aktif' : 'pasif'),
+                value: _status,
+                onChanged: (value) {
+                  setState(() {
+                    _status = value ?? true;
+                  });
+                },
+                controlAffinity: ListTileControlAffinity.leading,
+              ),
+
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: _submitForm,
+                child: const Text('ADD CATEGORY'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
