88692e4168888cbc004b18b3af370f7c9724323d
EsraYamanCode <yamaann.esra@gmail.com>
2026-08-18 08:53:57 +0300

    categoryId'ler değiştirildi.

--- lib/data/seed/category_seed_data.dart
+++ lib/data/seed/category_seed_data.dart
@@ -3,21 +3,21 @@ import '../models/category_model.dart';
 class CategorySeedData {
   static List<CategoryModel> initialCategories = [
     CategoryModel(
-      categoryId: "cat-1",
+      categoryId: "1",
       categoryTitle: "Kitap",
       categoryDescription: "Roman, bilim kurgu, kişisel gelişim kitapları",
       status: true,
       isDeleted: false,
     ),
     CategoryModel(
-      categoryId: "cat-2",
+      categoryId: "2",
       categoryTitle: "Kırtasiye",
       categoryDescription: "Defter, kalem ve ofis gereçleri",
       status: true,
       isDeleted: false,
     ),
     CategoryModel(
-      categoryId: "cat-2",
+      categoryId: "3",
       categoryTitle: "Elektronik",
       categoryDescription: "E-kitap okuyucu, kulaklık ve aksesuarlar",
       status: true,
